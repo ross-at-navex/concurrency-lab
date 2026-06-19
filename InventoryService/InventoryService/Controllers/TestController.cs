0835e2ae919583cd41e177d7fb338b7d5f108fb4
Ross Morrison <ross.morrison@navex.com>
2026-06-19 12:02:05 -0700

    Create test endpoint to invoke order endpoint in parallel

---
+++ InventoryService/InventoryService/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using InventoryService.Objects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpPost]
+    public async Task<int> OrderInParallel()
+    {
+        const string productName = "Cheese";
+        var client = new HttpClient();
+        var order = new ProductOrder { Quantity = 1 };
+        var orderProductUri = new Uri($"http://localhost:5266/inventory/{productName}");
+
+        var tasks = Enumerable.Range(0, 100).Select(_ => Task.Factory.StartNew(async () => await client.PatchAsJsonAsync(orderProductUri, order)));
+
+        await Task.WhenAll(tasks);
+
+        var checkStockUri = $"http://localhost:5266/inventory/{productName}";
+
+        return await client.GetFromJsonAsync<int>(checkStockUri);
+    }
+}
